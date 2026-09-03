b813ae759cf339528d504d30a894f657eea2dfec
Sourav Kumar <154826638+sourav23143@users.noreply.github.com>
2026-09-03 17:41:03 +0530

    Time: 3 ms (18.33%) | Memory: 19.6 MB (55.42%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.py
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.py
@@ -1,16 +1,20 @@
+#Time Complexity: O(n*n)
+#Space Complexity: O(1)
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
 
         n = len(mat)
         total_sum = 0
         for i in range(n):
+            for j in range(n):
             #adding primrary diagonal elements
-            total_sum += mat[i][i]
+                if i == j:
+                    total_sum += mat[i][j]
             #adding secondary diagonal_elements
-            total_sum += mat[i][n - 1 - i]
+                if i+j == n-1 and i != j:
+                    total_sum += mat[i][j]
 
-        if n % 2 != 0:
-            total_sum = total_sum - mat[n//2][n//2]
+       
         
         return total_sum
 
