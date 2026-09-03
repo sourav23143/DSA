3b104a05ab5d49ab24def57a63be2f48ae38c8be
Sourav Kumar <154826638+sourav23143@users.noreply.github.com>
2026-09-03 13:58:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (87.28%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,6 +1,11 @@
+#Time Complexity:  O(n)
+#Space Complexity: O(1)
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        #1. typecasting: converted x from int type to str type
         x = str(x)
+        #2. Comparision whether the original string and the reversed strings are same or not
         if x == x[::-1]:
             return True
 
