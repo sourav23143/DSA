4f18350d73c391c9b7f5d5640ede24f743fa93c6
Sourav Kumar <154826638+sourav23143@users.noreply.github.com>
2026-09-13 03:00:41 +0530

    Time: 25 ms (62.14%) | Memory: 33.5 MB (50.45%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -8,15 +8,17 @@ class Solution:
         slow = fast = nums[0]
         #Part 1: Finding the meeting point of the two pointer [slow and fast]
         while True:
+            #one step at a time
             slow = nums[slow]
+            #two step a time
             fast = nums[nums[fast]]
             if slow == fast:
                 break
         #Part 2: Find the starting point/entrance of the cycle
         slow = nums[0]
         while slow != fast:
-            slow = nums[slow]
-            fast = nums[fast]
+            slow = nums[slow] #one step
+            fast = nums[fast] #one step
         return fast   
 
 
